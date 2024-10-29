65f0a996098b4638d3be221e7ca8cb875c21b770
Duy Nhan <duynhannguyenn@gmail.com>
2024-10-29 18:49:32 +0700

    feat(custom-link): create custom link

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -5,6 +5,7 @@ import React from "react";
 import delay from "delay";
 import IssueActions from "./IssueActions";
 import Link from "next/link";
+import CustomLink from "@/components/CustomLink";
 const IssuesPage = async () => {
   const getIssues = await prisma.issue.findMany();
   await delay(2000);
@@ -28,7 +29,10 @@ const IssuesPage = async () => {
           {getIssues.map((issue) => (
             <Table.Row key={issue.id}>
               <Table.Cell>
-                <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
+                {/* <Link href={`/issues/${issue.id}`}>{issue.title}</Link> */}
+                <CustomLink href={`issues/${issue.id}`}>
+                  {issue.title}
+                </CustomLink>
                 <div className="md:hidden block">
                   <IssueStatusBadge status={issue.status} />
                 </div>
---
+++ components/CustomLink.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import NextLink from "next/link";
+import { Link as RadixLink } from "@radix-ui/themes";
+
+type CustomLinkProps = {
+  href: string;
+  children: string;
+};
+
+const CustomLink = ({ href, children }: CustomLinkProps) => {
+  return (
+    <NextLink href={href} legacyBehavior passHref>
+      <RadixLink>{children}</RadixLink>
+    </NextLink>
+  );
+};
+
+export default CustomLink;
