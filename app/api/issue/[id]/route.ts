53439684be4e7de96ea6ed7360c04e639030da67
Duy Nhan <duynhannguyenn@gmail.com>
2024-10-30 19:53:36 +0700

    feat(edit issue api): Build edit issue api

---
+++ app/api/issue/[id]/route.ts
@@ -0,0 +1,33 @@
+import { updateIssueSchema } from "@/app/validationSchema";
+import prisma from "@/prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function PATCH(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const body = await request.json();
+  const validation = updateIssueSchema.safeParse(body);
+
+  if (!validation.success) {
+    return NextResponse.json(validation.error.format(), { status: 400 });
+  }
+
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+
+  if (!issue)
+    return NextResponse.json({ error: "Invalid issue" }, { status: 404 });
+
+  const updatedIssue = await prisma.issue.update({
+    where: { id: issue.id },
+    data: {
+      title: body.title,
+      description: body.description,
+      status: body.status,
+    },
+  });
+
+  return NextResponse.json(updatedIssue);
+}
--- app/validationSchema.ts
+++ app/validationSchema.ts
@@ -4,3 +4,8 @@ export const createIssueSchema = z.object({
   title: z.string().min(1, "Title is required ").max(255),
   description: z.string().min(1, "Description is required"),
 });
+export const updateIssueSchema = z.object({
+  title: z.string().min(1, "Title is required ").max(255),
+  description: z.string().min(1, "Description is required"),
+  status: z.enum(["OPEN", "IN_PROGRESS", "CLOSED"]),
+});
