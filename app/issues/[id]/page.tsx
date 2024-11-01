9a0d1f8d963058bb25d44605b5f61cf747934ec7
Duy Nhan <duynhannguyenn@gmail.com>
2024-11-01 19:12:16 +0700

    feat(delete button): add delete button

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,9 +1,10 @@
 import prisma from "@/prisma/client";
-import { Box, Grid } from "@radix-ui/themes";
+import { Box, Flex, Grid } from "@radix-ui/themes";
 
 import { notFound } from "next/navigation";
 import EditIssueButton from "./EditIssueButton";
 import IssueDetails from "./IssueDetails";
+import DeleteIssueButton from "./DeleteIssueButton";
 type Props = {
   params: { id: string };
 };
@@ -18,14 +19,20 @@ const IssueDetailPage = async ({ params }: Props) => {
 
   return (
     <Grid
-      columns={{ initial: "1", md: "2" }}
+      columns={{ initial: "1", sm: "5" }}
       gap={"5"}
     >
-      <Box>
+      <Box className="md:col-span-4">
         <IssueDetails issue={getIssueDetails} />
       </Box>
       <Box>
-        <EditIssueButton issueId={getIssueDetails.id} />
+        <Flex
+          direction={"column"}
+          gap={"4"}
+        >
+          <EditIssueButton issueId={getIssueDetails.id} />
+          <DeleteIssueButton issueId={getIssueDetails.id} />
+        </Flex>
       </Box>
     </Grid>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import NavBar from "./NavBar";
-import { Theme } from "@radix-ui/themes";
+import { Container, Theme } from "@radix-ui/themes";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
@@ -21,9 +21,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.variable}>
-        <Theme accentColor="amber" grayColor="gray">
+        <Theme
+          accentColor="amber"
+          grayColor="gray"
+        >
           <NavBar />
-          <main className="p-5">{children}</main>
+          <main className="p-5">
+            <Container>{children}</Container>
+          </main>
         </Theme>
       </body>
     </html>
