9746bcd3711d4b766ff6ee4f9cbe29653bdefcbb
Duy Nhan <duynhannguyenn@gmail.com>
2024-11-04 03:41:29 +0700

    feat(auth): adding login, logout link

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -5,14 +5,15 @@ import { usePathname } from "next/navigation";
 import React from "react";
 import { FaBug } from "react-icons/fa6";
 import classNames from "classnames";
+import { Box } from "@radix-ui/themes";
+import { useSession } from "next-auth/react";
 const NavBar = () => {
   const links = [
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues" },
   ];
-
+  const { status, data: session } = useSession();
   const currentPath = usePathname();
-
   return (
     <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
       <Link href="/">
@@ -21,19 +22,28 @@ const NavBar = () => {
       </Link>
       <ul className="flex space-x-6 ">
         {links.map((link) => (
-          <Link
-            key={link.href}
-            className={classNames({
-              "text-slate-300": link.href === currentPath,
-              "text-zinc-500": link.href !== currentPath,
-              "hover:text-slate-100 transition-colors": true,
-            })}
-            href={link.href}
-          >
-            {link.label}
-          </Link>
+          <li key={link.href}>
+            <Link
+              className={classNames({
+                "text-slate-300": link.href === currentPath,
+                "text-zinc-500": link.href !== currentPath,
+                "hover:text-slate-100 transition-colors": true,
+              })}
+              href={link.href}
+            >
+              {link.label}
+            </Link>
+          </li>
         ))}
       </ul>
+      <Box>
+        {status === "authenticated" && (
+          <Link href={"/api/auth/signout"}>Log out</Link>
+        )}
+        {status === "unauthenticated" && (
+          <Link href={"/api/auth/signin"}>Login</Link>
+        )}
+      </Box>
     </nav>
   );
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import NavBar from "./NavBar";
 import { Container, Theme } from "@radix-ui/themes";
+import AuthProvider from "./Provider";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
@@ -21,15 +22,17 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.variable}>
-        <Theme
-          accentColor="amber"
-          grayColor="gray"
-        >
-          <NavBar />
-          <main className="p-5">
-            <Container>{children}</Container>
-          </main>
-        </Theme>
+        <AuthProvider>
+          <Theme
+            accentColor="amber"
+            grayColor="gray"
+          >
+            <NavBar />
+            <main className="p-5">
+              <Container>{children}</Container>
+            </main>
+          </Theme>
+        </AuthProvider>
       </body>
     </html>
   );
