02cd5735b267b51532e6f32f149eb220298a1ce4
Duy Nhan <duynhannguyenn@gmail.com>
2024-11-18 12:32:48 +0700

    feat(issuePage): Make columns sortable

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -4,12 +4,28 @@ import { Table } from "@radix-ui/themes";
 import React from "react";
 import IssueActions from "./IssueActions";
 import CustomLink from "@/components/CustomLink";
-import { Status } from "@prisma/client";
+import { Issue, Status } from "@prisma/client";
+import Link from "next/link";
+import { ArrowDownIcon, ArrowUpIcon } from "@radix-ui/react-icons";
 const IssuesPage = async ({
   searchParams,
 }: {
-  searchParams: { status: Status };
+  searchParams: { status: Status; orderBy: keyof Issue; dir: "asc" | "desc" };
 }) => {
+  const columns: {
+    title: string;
+    value: keyof Issue;
+    className?: string;
+  }[] = [
+    { title: "Issues", value: "title" },
+    { title: "Status", value: "status", className: "hidden md:table-cell" },
+    {
+      title: "Created",
+      value: "createdAt",
+      className: "hidden md:table-cell ",
+    },
+  ];
+
   const statuses = Object.values(Status);
   const status = statuses.includes(searchParams.status)
     ? searchParams.status
@@ -18,19 +34,49 @@ const IssuesPage = async ({
   const getIssues = await prisma.issue.findMany({
     where: { status },
   });
+
   return (
     <div>
       <IssueActions />
       <Table.Root variant="surface">
         <Table.Header>
           <Table.Row>
-            <Table.ColumnHeaderCell>Issues</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className="hidden md:table-cell ">
-              Status
-            </Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className="hidden md:table-cell ">
-              Created
-            </Table.ColumnHeaderCell>
+            {columns.map((column) => (
+              <Table.ColumnHeaderCell
+                className={column.className}
+                key={column.value}
+              >
+                <Link
+                  href={
+                    searchParams.dir === "asc"
+                      ? {
+                          query: {
+                            ...searchParams,
+                            orderBy: column.value,
+                            dir: "desc",
+                          },
+                        }
+                      : {
+                          query: {
+                            ...searchParams,
+                            orderBy: column.value,
+                            dir: "asc",
+                          },
+                        }
+                  }
+                >
+                  {column.title}{" "}
+                </Link>
+                {column.value === searchParams.orderBy &&
+                  searchParams.dir === "asc" && (
+                    <ArrowUpIcon className="inline font-bold " />
+                  )}
+                {column.value === searchParams.orderBy &&
+                  searchParams.dir === "desc" && (
+                    <ArrowDownIcon className="inline" />
+                  )}
+              </Table.ColumnHeaderCell>
+            ))}
           </Table.Row>
         </Table.Header>
 
